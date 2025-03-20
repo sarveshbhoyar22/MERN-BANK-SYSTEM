97a6c5e9f1c442cdf83f7dea67d3e05820c190c0
SarveshBhoyar <sarveshkbhoyar2022@gmail.com>
2025-03-21 03:10:05 +0530

    login update

--- frontend/src/hooks/UseLogin.jsx
+++ frontend/src/hooks/UseLogin.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL || "http://localhost:5000";
-const API_BASE_URL = `${BASE_URL}/api`;
+const API_BASE_URL = `${BASE_URL}`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,

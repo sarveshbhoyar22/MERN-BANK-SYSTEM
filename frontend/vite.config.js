b94b7821cdc239820978bba074db7de5ef81c2f3
sarveshbhoyar22 <sarveshkbhoyar2022@gmail.com>
2025-07-12 02:13:49 +0530

    vercelupdate

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,18 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server:{
-    port:3000,
-    proxy:{
-      "/api":{
-        target:"http://localhost:5000",
+  server: {
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
         // changeOrigin:true
-      }
-    }
-  }
+      },
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
 });
 
 
